c427d97349d510ff9f93e8cbc539cfb2077b9bc3
BassemYagoub <yagoub.bassem@gmail.com>
2022-01-28 16:09:33 +0100

    Fixed dash bug + Added atomatic regen HP for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,19 +25,23 @@ public class PlayerController : MonoBehaviour {
     private Gradient reloadGradient;
 
     [Header("Player properties")]
-    public float health = 3f;
+    public float maxHealth = 3f;
+    public float health;
     public float damage = 1f;
     public float dashSpeed = 1f;
-    public float recoveryFrame = .5f;
-    private float impactTime = -1f;
+    public float invinciblityFrame = .5f;
+    public float regenDelay = 5f;
+    private float lastImpactTime = -1f;
     private Vector3 targetPos;
+    private bool dashing = false;
 
     public GameObject rightHand;
-    //public GameObject impactEffect;
 
 
 
     void Start() {
+        health = maxHealth;
+
         //left hand ray color management
         leftHandLineVisual = leftHandOrigin.parent.GetComponent<XRInteractorLineVisual>();
         validGradient = leftHandLineVisual.validColorGradient;
@@ -55,8 +59,9 @@ public class PlayerController : MonoBehaviour {
         if (health > 0 && !gamePaused) {
             //move left or right
             DashTowardsTarget();
+            AutomaticHPRegen();
 
-            if (moveReference.action.triggered) {
+            if (moveReference.action.triggered && !dashing) {
                 UpdateDashPosition(moveReference.action.ReadValue<Vector2>());
             }
 
@@ -122,6 +127,7 @@ public class PlayerController : MonoBehaviour {
         Vector3 pos = transform.position;
         float maxPos = 2.4f;
         float step = dashSpeed*Time.deltaTime;
+        dashing = true;
 
         if (joystickVect.x > 0) { //go right
             if(pos.x < maxPos) {
@@ -145,6 +151,9 @@ public class PlayerController : MonoBehaviour {
             targetPos = new Vector3(targetPos.x, transform.position.y, transform.position.z);
             transform.position = Vector3.MoveTowards(transform.position, targetPos, dashSpeed * Time.deltaTime);
         }
+        else {
+            dashing = false;
+        }
     }
 
     /// <summary>
@@ -155,6 +164,10 @@ public class PlayerController : MonoBehaviour {
         projectileObj.GetComponent<Rigidbody>().velocity = ((leftHandOrigin.rotation * Vector3.forward)).normalized * projectileSpeed;
     }
 
+    /// <summary>
+    /// Player takes damage after hitting something
+    /// </summary>
+    /// <param name="collision">the object that hit the player</param>
     private void OnCollisionEnter(Collision collision) {
         Debug.Log("collision with "+ collision.gameObject.name);
         if (collision.gameObject.tag == "Enemy" || collision.gameObject.tag == "Obstacle") {
@@ -162,17 +175,42 @@ public class PlayerController : MonoBehaviour {
         }
     }
 
+    /// <summary>
+    /// Updates health shown ingame
+    /// </summary>
+    void UpdateHealthUI() {
+        nbLivesUI.text = health.ToString();
+    }
+
+    /// <summary>
+    /// Deals a certain amount of damage to player
+    /// </summary>
+    /// <param name="amount">the amount of damage to deal</param>
     public void TakeDamage(float amount) {
-        if (Time.time > impactTime+recoveryFrame) { //if not recovering => take damage
-            impactTime = Time.time;
+        if (Time.time > lastImpactTime + invinciblityFrame) { //if not recovering => take damage
+            lastImpactTime = Time.time;
             health -= amount;
-            nbLivesUI.text = health.ToString();
+            UpdateHealthUI();
+
             if (health <= 0f) {
                 Die();
             }
         }
     }
 
+    /// <summary>
+    /// Regenerates 1 HP for player after regenDelay seconds
+    /// </summary>
+    void AutomaticHPRegen() {
+        if (Time.time > lastImpactTime + regenDelay && health < maxHealth) {
+            health++;
+            UpdateHealthUI();
+        }
+    }
+
+    /// <summary>
+    /// Pauses everything ingame when player loses
+    /// </summary>
     void Die() {
         Time.timeScale = 0; //stop all external movements/animations
         rightHand.GetComponent<XRRayInteractor>().enabled = true;
